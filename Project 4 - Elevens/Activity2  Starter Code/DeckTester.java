08d7afca54416981bb986ebe86ddf2a0d341de0c
amariet <785037@seq.org>
2017-01-18 22:25:08 -0800

    Update DeckTester.java

--- Project 4 - Elevens/Activity2  Starter Code/DeckTester.java
+++ Project 4 - Elevens/Activity2  Starter Code/DeckTester.java
@@ -11,6 +11,19 @@ public class DeckTester {
 		String[] firstRank = {"King", "Queen", "Jack"};
 		String[] firstSuit = {"Hearts", "Spades", "Diamonds"};
 		int[] firstValue = {9, 10, 12};
-		Deck first Deck = new Deck(firstRank, firstSuit, firstValue);
+		Deck firstDeck = new Deck(firstRank, firstSuit, firstValue);
+	        System.out.println("This deck must have a size should of 3: " + firstDeck.size());
+	    
+	        String[] secondRank = {"King", "12", "Queen"};
+	        String[] secondSuit= {"Clubs", "Hearts", "Diamonds"};
+	        int[] secondValue = {1, 9, 13};
+	        Deck secondDeck = new Deck(secondRank, secondSuit, secondValue);
+	        System.out.println("This deck should be filled: " + !secondDeck.isEmpty());
+	    
+	        String[] thirdRank = {"King", "6", "2"};
+	        String[] thirdSuit = {"Spades", "Diamonds", "Hearts"};
+	        int[] thirdValue = {11, 4, 6};
+	        Deck thirdDeck = new Deck(thirdRank, thirdSuit, thirdValue);
+	        System.out.println("This card has dealt this card: " + thirdDeck.deal());
 	}
 }
